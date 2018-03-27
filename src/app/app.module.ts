d6e4891a5886039a0b4eabff5d6b9c3c9f625f46
sawyerbutton <lil103@pitt.edu>
2018-03-27 17:24:07 -0400

    deleting unecessary content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NomeService } from './shared/services/nomes.service';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { MenuComponent } from './shared/components/menu/menu.component';
 import { PageComponent } from './shared/components/page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MenuComponent,
     PageComponent
   ],
   imports: [
@@ -24,6 +20,6 @@ import { PageComponent } from './shared/components/page/page.component';
   providers: [NomeService],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
+export class AppModule {
 
 }
